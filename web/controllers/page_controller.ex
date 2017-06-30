b56df15ba94246b1b08cfbc0cdb49d0992aedc03
Adi Iyengar <aditya7iyengar@gmail.com>
2017-06-30 15:58:01 -0400

    Make page controller link to first question

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule Scavgather.PageController do
   use Scavgather.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    question = Scavgather.Repo.get!(Scavgather.Question, 1)
+    render conn, "index.html",
+      question: question
   end
 end
